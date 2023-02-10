f7539ec925436b5383de8e2010b3d173304b5f79
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-02-10 16:52:30 +0330

    chore: config mantine provider app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
+import { Button } from '@mantine/core'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={inter.className}>
-
+        <Button>
+          Settings
+        </Button>
       </main>
     </>
   )
