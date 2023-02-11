f129daf855b186d68f518a59800d8f86253a094d
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-02-11 20:58:02 +0330

    chore: responsive Header

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -2,11 +2,29 @@ import { Flex, Title, Input, Button } from "@mantine/core"
 
 const Header = () => {
     return (
-        <Flex align="center" justify="space-between" p={16} >
-            <Title order={2}>Next Image Gallery</Title>
+        <Flex
+            align="center"
+            justify="space-between"
+            sx={{
+                flexDirection: "row",
+                '@media (max-width: 900px)': {
+                    flexDirection: "column"
+                }
+            }}
+            p={16} >
+            <Title order={2} sx={{
+                '@media (max-width: 900px)': {
+                    marginBottom: 12,
+                },
+            }}>Next Image Gallery</Title>
             <Flex align="center" justify="center">
                 <Input
-                    sx={{ width: "500px" }}
+                    sx={{
+                        width: "500px",
+                        '@media (max-width: 755px)': {
+                            width: "240px",
+                        },
+                    }}
                     placeholder="Search Picture"
                     radius="md"
                     size="md"
@@ -18,7 +36,7 @@ const Header = () => {
                     gradient={{ from: '#ed6ea0', to: '#ec8c69', deg: 35 }}
                 >Search</Button>
             </Flex>
-        </Flex>
+        </Flex >
     )
 }
 
